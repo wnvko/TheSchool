38fc1e137cfcd44b30e17e4470ec65a726d7b83b
wnvko <milko.venkov@gmail.com>
2016-02-23 01:52:31 +0200

    Voting ready with unobtrusive JS and AJAX

--- Web/TheSchool.Web/Controllers/NewsController.cs
+++ Web/TheSchool.Web/Controllers/NewsController.cs
@@ -2,17 +2,21 @@
 {
     using System.Linq;
     using System.Web.Mvc;
+    using Data.Models;
     using Infrastructure.Mapping;
+    using Microsoft.AspNet.Identity;
     using Services.Data;
     using ViewModels.News;
 
     public class NewsController : Controller
     {
         private readonly INewsService news;
+        private readonly IVotesService votes;
 
-        public NewsController(INewsService news)
+        public NewsController(INewsService news, IVotesService votes)
         {
             this.news = news;
+            this.votes = votes;
         }
 
         public ActionResult Details(int id)
@@ -23,6 +27,31 @@
                 .To<NewsViewModel>()
                 .FirstOrDefault();
             return this.View(news);
+        }
+
+        [Authorize]
+        [HttpPost]
+        public ActionResult Vote(int id)
+        {
+            var userId = this.User.Identity.GetUserId();
+            var vote = this.votes
+                .All()
+                .FirstOrDefault(v => v.VoterId == userId && v.NewsId == id);
+
+            if (vote == null)
+            {
+                var newVote = new Vote();
+                newVote.NewsId = id;
+                newVote.VoterId = userId;
+                this.votes.Add(newVote);
+            }
+
+            var votesCount = this.news
+                .All()
+                .FirstOrDefault(n => n.Id == id)
+                .Votes.Count();
+
+            return this.Json(new { Count = votesCount });
         }
     }
 }
