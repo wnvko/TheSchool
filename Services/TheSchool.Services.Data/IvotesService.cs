de9660cde7096795b7f7dd03d7f2418c848d7a6c
wnvko <milko.venkov@gmail.com>
2016-02-23 01:55:02 +0200

    Clear some stuff - Tool/Crawler

---
+++ Services/TheSchool.Services.Data/IvotesService.cs
@@ -0,0 +1,12 @@
+﻿namespace TheSchool.Services.Data
+{
+    using System.Linq;
+    using TheSchool.Data.Models;
+
+    public interface IVotesService
+    {
+        Vote Add(Vote vote);
+
+        IQueryable<Vote> All();
+    }
+}
---
+++ Services/TheSchool.Services.Data/VotesService.cs
@@ -0,0 +1,28 @@
+﻿namespace TheSchool.Services.Data
+{
+    using System.Linq;
+    using TheSchool.Data.Common;
+    using TheSchool.Data.Models;
+
+    public class VotesService : IVotesService
+    {
+        private readonly IDbRepositoryInty<Vote> votes;
+
+        public VotesService(IDbRepositoryInty<Vote> votes)
+        {
+            this.votes = votes;
+        }
+
+        public Vote Add(Vote vote)
+        {
+            this.votes.Add(vote);
+            this.votes.Save();
+            return vote;
+        }
+
+        public IQueryable<Vote> All()
+        {
+            return this.votes.All();
+        }
+    }
+}
