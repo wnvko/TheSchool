4e8bb91d52604d629c2deabf6fdb687ec4cd4434
wnvko <milko.venkov@gmail.com>
2016-02-23 22:26:01 +0200

    Some tests added

---
+++ Tests/TheSchool.Web.Controllers.Tests/TheSchoolControllerTests.cs
@@ -0,0 +1,32 @@
+﻿namespace TheSchool.Web.Controllers.Tests
+{
+    using System.Reflection;
+    using System.Web.Mvc;
+    using Controllers;
+    using Infrastructure.Mapping;
+    using Moq;
+    using NUnit.Framework;
+    using Services.Data;
+
+    [TestFixture]
+
+    public class TheSchoolControllerTests
+    {
+        [Test]
+        public void HomeIndexReturnsCorectView()
+        {
+            /*
+             var autoMapperConfig = new AutoMapperConfig();
+             autoMapperConfig.Execute(Assembly.GetAssembly(typeof(HomeController)));
+
+             Mock<IStudentsService> students = new Mock<IStudentsService>();
+             Mock<INewsService> news = new Mock<INewsService>();
+
+             var contoller = new HomeController(students.Object, news.Object);
+             var result = contoller.Index() as ViewResult;
+
+             Assert.AreEqual("Index", result.ViewName);
+             */
+        }
+    }
+}
---
+++ Tests/TheSchool.Web.Routes.Tests/TheSchoolRouteTests.cs
@@ -0,0 +1,77 @@
+﻿namespace TheSchool.Web.Routes.Tests
+{
+    using System.Web.Routing;
+    using Controllers;
+    using MvcRouteTester;
+    using NUnit.Framework;
+    using ViewModels.News;
+
+    [TestFixture]
+    public class TheSchoolRouteTests
+    {
+        [Test]
+        public void TestNewsDetailsRouteById()
+        {
+            var routesCollection = new RouteCollection();
+            const string url = @"/News/Details/95";
+
+            RouteConfig.RegisterRoutes(routesCollection);
+
+            routesCollection.ShouldMap(url)
+                .To<NewsController>(
+                    c => c.Details(95));
+        }
+
+        [Test]
+        public void TestNewsIndexRouteById()
+        {
+            var routesCollection = new RouteCollection();
+            const string url = @"/News/Index/2";
+
+            RouteConfig.RegisterRoutes(routesCollection);
+
+            routesCollection.ShouldMap(url)
+                .To<NewsController>(
+                    c => c.Index(2));
+        }
+
+        [Test]
+        public void TestNewsAddNewsGetRoute()
+        {
+            var routesCollection = new RouteCollection();
+            const string url = @"/News/AddNews";
+
+            RouteConfig.RegisterRoutes(routesCollection);
+
+            routesCollection.ShouldMap(url)
+                .To<NewsController>(
+                    c => c.AddNews());
+        }
+
+        [Test]
+        public void TestNewsAddNewsPostRoute()
+        {
+            var routesCollection = new RouteCollection();
+            const string url = @"/News/AddNews";
+
+            RouteConfig.RegisterRoutes(routesCollection);
+
+            routesCollection.ShouldMap(url)
+                .To<NewsController>(
+                    c => c.AddNews(new NewsInputModel()));
+        }
+
+        [Test]
+        public void TestHomeIndexRoute()
+        {
+            var routesCollection = new RouteCollection();
+            const string url = @"/Home/Index";
+
+            RouteConfig.RegisterRoutes(routesCollection);
+
+            routesCollection.ShouldMap(url)
+                .To<HomeController>(
+                    c => c.Index());
+        }
+    }
+}
