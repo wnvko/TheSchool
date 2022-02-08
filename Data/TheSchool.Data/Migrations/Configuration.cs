fbe3ef361066474b72b0765d619c38f802909eb8
Milko Venkov <MVenkov@infragistics.com>
2022-02-09 00:30:45 +0200

    Update The School

--- Data/TheSchool.Data/Migrations/Configuration.cs
+++ Data/TheSchool.Data/Migrations/Configuration.cs
@@ -19,26 +19,26 @@
 
         protected override void Seed(ApplicationDbContext context)
         {
-            const string AdministratorUserName = "admin@admin.com";
-            const string AdministratorPassword = AdministratorUserName;
+            //const string AdministratorUserName = "admin@admin.com";
+            //const string AdministratorPassword = AdministratorUserName;
 
-            if (!context.Roles.Any())
-            {
-                // Create admin role
-                var roleStore = new RoleStore<IdentityRole>(context);
-                var roleManager = new RoleManager<IdentityRole>(roleStore);
-                var role = new IdentityRole { Name = GlobalConstants.AdministratorRoleName };
-                roleManager.Create(role);
+            //if (!context.Roles.Any())
+            //{
+            //    // Create admin role
+            //    var roleStore = new RoleStore<IdentityRole>(context);
+            //    var roleManager = new RoleManager<IdentityRole>(roleStore);
+            //    var role = new IdentityRole { Name = GlobalConstants.AdministratorRoleName };
+            //    roleManager.Create(role);
 
-                // Create admin user
-                var userStore = new UserStore<ApplicationUser>(context);
-                var userManager = new UserManager<ApplicationUser>(userStore);
-                var user = new ApplicationUser { UserName = AdministratorUserName, Email = AdministratorUserName };
-                userManager.Create(user, AdministratorPassword);
+            //    // Create admin user
+            //    var userStore = new UserStore<ApplicationUser>(context);
+            //    var userManager = new UserManager<ApplicationUser>(userStore);
+            //    var user = new ApplicationUser { UserName = AdministratorUserName, Email = AdministratorUserName };
+            //    userManager.Create(user, AdministratorPassword);
 
-                // Assign user to admin role
-                userManager.AddToRole(user.Id, GlobalConstants.AdministratorRoleName);
-            }
+            //    // Assign user to admin role
+            //    userManager.AddToRole(user.Id, GlobalConstants.AdministratorRoleName);
+            //}
         }
     }
 }
